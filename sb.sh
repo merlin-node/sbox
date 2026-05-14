bea93b3025c4e27263d5ce9f3d29f7aea6146020
Merlin <70049158+merlin-node@users.noreply.github.com>
2026-05-14 18:37:45 +0800

    Add files via upload

--- sb.sh
+++ sb.sh
@@ -11,7 +11,7 @@ set -o pipefail
 
 SCRIPT_VERSION="1.2"
 SCRIPT_AUTHOR="Merlin"
-SCRIPT_UPDATE_URL="https://example.com/sb.sh"
+SCRIPT_UPDATE_URL="https://raw.githubusercontent.com/merlin-node/sbox/main/sb.sh"
 
 RED='\033[0;31m'; GREEN='\033[0;32m'; YELLOW='\033[0;33m'
 BLUE='\033[0;34m'; CYAN='\033[0;36m'; MAGENTA='\033[0;35m'
